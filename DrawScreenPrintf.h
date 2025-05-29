6e4937a3bfc196cb0aabcc1faf107d08bde95d74
saitoA1007 <k024g0004@g.neec.ac.jp>
2025-05-29 22:10:34 +0900

    MT4_1_3_確認課題

--- DrawScreenPrintf.h
+++ DrawScreenPrintf.h
@@ -1,6 +1,7 @@
 #pragma once
 #include"Vector3.h"
 #include"Matrix4x4.h"
+#include"Math.h"
 
 static const int kRowHeight = 20;
 static const int kColumnWidth = 60;
@@ -8,3 +9,5 @@ static const int kColumnWidth = 60;
 void VectorScreenPrintf(int x, int y, const Vector3& vector, const char* label);
 
 void MatrixScreenPrintf(int x, int y, const Matrix4x4& matrix, const char* label);
+
+void QuaternionScreenPrintf(int x, int y, const Quaternion& quaternion, const char* label);
